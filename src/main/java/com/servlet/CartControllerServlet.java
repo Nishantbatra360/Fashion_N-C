8189ee58b765102a81f681ee472fba9fcdb5e3d7
Nishantbatra360 <nishantbatra360@gmail.com>
2021-05-06 20:25:45 -0400

    changes

--- src/main/java/com/servlet/CartControllerServlet.java
+++ src/main/java/com/servlet/CartControllerServlet.java
@@ -1,41 +1,43 @@
 package com.servlet;
 
 import java.io.IOException;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
-/**
- * Servlet implementation class CartControllerServlet
- */
 @WebServlet("/CartControllerServlet")
 public class CartControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private CartDbUtil cartDbUtil;
+	
+	@Resource(name="jdbc/ferme_bd")
+	private DataSource dataSource;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		
+		try {
+			cartDbUtil = new CartDbUtil(dataSource);
+		}
+		catch (Exception exc) {
+			throw new ServletException(exc);
+		}
+	}
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CartControllerServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
